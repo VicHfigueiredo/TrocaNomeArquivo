4eb9e958b25e9d5b31deafe760d6bd0cf32bb242
victor.hfigueiredo <victor.hfigueiredo@SBC0505249W10-1>
2022-05-13 17:15:13 -0300

    Adicionar arquivos de projeto.

---
+++ TrocaNomeArquivo/View/TelaPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaNomeArquivo.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            tbxEndereco.Text = string.Empty;
+
+            try
+            {
+                if(folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    tbxEndereco.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+    }
+}
